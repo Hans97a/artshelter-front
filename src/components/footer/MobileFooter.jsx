fa19e0ef3a9df858871ef1f004b3a31abe41939b
bokyeal kim <byby8992@gmail.com>
2023-01-15 15:27:11 +0900

    mobile footer update

--- src/components/footer/MobileFooter.jsx
+++ src/components/footer/MobileFooter.jsx
@@ -12,7 +12,40 @@ export default function MobileFooter() {
   return (
     <>
       <Box w="100%" h="20%" bgColor="blackAlpha.800">
-        <Box display="flex" justifyContent="center">
+        <Box>
+          <Box pt="1%" ml="2rem">
+            <Text color="green" fontWeight="bold" fontSize="1.5rem" mt="0.5rem">
+              ART SHELTER
+            </Text>
+            <Box>
+              <Box color="white" mt="0.5rem" fontSize="0.8rem">
+                <Text>
+                  아트 쉘터{" "}
+                  <Box as="span" ml="3px" mr="3px">
+                    |
+                  </Box>{" "}
+                  사업자등록번호 : 769-99-01174{" "}
+                  <Box as="span" ml="3px">
+                    |
+                  </Box>
+                </Text>
+                <Text>
+                  대표 : 김지후{" "}
+                  <Box as="span" ml="3px" mr="3px">
+                    |
+                  </Box>
+                  서울 마포구 성미산로 31길 11-14
+                </Text>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        <Box mt="0.5rem" pb="1rem" ml="2rem">
+          <Box color="white" fontSize="0.8rem">
+            <Text>Copyright © 2022 ART SHELTER. Powered by ART SHELTER</Text>
+          </Box>
+        </Box>
+        {/* <Box display="flex" justifyContent="center">
           <Box pt="1%">
             <Text color="green" fontWeight="bold" fontSize="1.5rem" mt="0.5rem">
               ART SHELTER
@@ -45,7 +78,7 @@ export default function MobileFooter() {
             <Text>Copyright © 2022 ART SHELTER</Text>
             <Text>Powered by ART SHELTER</Text>
           </Box>
-        </Box>
+        </Box> */}
       </Box>
     </>
   );
